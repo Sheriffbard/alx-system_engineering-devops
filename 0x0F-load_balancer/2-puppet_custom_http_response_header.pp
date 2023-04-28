d4650c9c365c536f736333e060ea0bd98fe978f2
root <olawuyiola@gmail.com>
2023-04-28 06:31:01 -0700

    load_balancer

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,9 @@
+# to create  a custom HTTP header response
+
+exec { 'command':
+  command  => 'apt-get -y update;
+  apt-get -y install nginx;
+  sudo sed -i "/listen 80 default_server;/a add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-available/default;
+  sudo service nginx restart',
+  provider => shell,
+}
